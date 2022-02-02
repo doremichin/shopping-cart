5f21d3c5aa4096fb21d3e71e2fa4429e32a0a9e9
doremichin <johnkim994@naver.com>
2022-02-02 14:58:51 +0900

    [CT-1] 장바구니에 상품이 들어있다면 header안 카트아이콘에 표시하기

--- src/views/shared/Header/Nav.tsx
+++ src/views/shared/Header/Nav.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 import {Link} from "react-router-dom";
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 
-function Nav () {
+interface Props {
+    qty : number
+}
+
+function Nav ({qty} : Props) {
     return(
         <Container>
             <NavItem to={'/'}>home</NavItem>
-            <NavItem to={'/cart'}>
+            <CartNavItem to={'/cart'} qty={qty}>
                 <Icon>
                     <AiOutlineShoppingCart/>
                 </Icon>
-            </NavItem>
+            </CartNavItem>
         </Container>
     )
 };
@@ -21,11 +25,32 @@ const Container = styled.div`
   display: flex;
   align-items: center;
 `;
-const NavItem = styled(Link)`
+export const NavItemStyle = styled(Link)`
+  position: relative;
   padding: 10px 7px;
   margin-left: 20px;
   color: #000;
   text-decoration: none;
+`;
+const NavItem = styled(NavItemStyle)`
+    
+`;
+const CartNavItem = styled(NavItemStyle)<{qty : number}>`
+  ${(props) => (props?.qty > 0 ? css`
+    &::after{
+      content: '';
+      position: absolute;
+      top: 8px;
+      right: 3px;
+      width: 10px;
+      height: 10px;
+      border-radius: 50%;
+      background-color:red;
+    }
+  ` : css`
+  
+  `)}
+
 `;
 const Icon = styled.div`
   font-size: 20px;
