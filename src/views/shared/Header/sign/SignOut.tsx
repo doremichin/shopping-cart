7b538d1aeb941450218fd9d885809c2707b32215
doremichin <johnkim994@naver.com>
2022-02-11 14:16:58 +0900

    [Auth] google OAuth sign in, sign out 기능 추가
    sign in, sign out 버튼 추가

---
+++ src/views/shared/Header/sign/SignOut.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from '@mui/material/Button';
+import { useRecoilState, useResetRecoilState } from 'recoil';
+
+import { useGoogleLogout } from 'react-google-login';
+
+import { authCheckerAtom, authDataAtom } from '../../../../atoms/authAtom';
+
+function SignOut() {
+  const [authCheck, setAuthCheck] = useRecoilState(authCheckerAtom);
+  const resetAuthData = useResetRecoilState(authDataAtom);
+
+  const onLogoutSuccess = () => {
+    setAuthCheck(false);
+    resetAuthData();
+  };
+  const { signOut, loaded } = useGoogleLogout({
+    clientId: '896627521201-rdfc3vn6jqjq858ac6qo904oiaa9oiqc.apps.googleusercontent.com',
+    cookiePolicy: 'single_host_origin',
+    onLogoutSuccess,
+  });
+  return (
+    <Button color="inherit" onClick={signOut}>Logout</Button>
+
+  );
+}
+
+const Container = styled.div`
+
+`;
+
+export default SignOut;
