58a658dc68cf51ea8c74e0fe75caa4f029d8db5c
doremichin <johnkim994@naver.com>
2022-02-21 14:15:28 +0900

    [Add] 새 상품 데이터를 db에 set 하는 로직 추가

--- src/firebase/query.ts
+++ src/firebase/query.ts
@@ -1,29 +1,26 @@
 import {
-  collection, getDocs, query,
+  collection, getDocs, query, doc, setDoc,
 } from 'firebase/firestore';
 
 import { db } from './initialize';
+import { FormInputs } from '../views/add/components/AddProductForm';
 
 export const getProductsFirebase = async () => {
-  // const docRef = doc(db, 'products');
-  // const docSnap = await getDoc(docRef);
-  //
-  // if (docSnap.exists()) {
-  //   console.log('Document data:', docSnap.data());
-  // } else {
-  //   // doc.data() will be undefined in this case
-  //   console.log('No such document!');
-  // }
   const q = query(collection(db, 'products'));
   const querySnapshot = await getDocs(q);
   const result : any[] = [];
-  querySnapshot.forEach((doc) => {
+  querySnapshot.forEach((document) => {
     const docItem : any = {
-      id: doc.id,
-      ...doc.data(),
+      id: document.id,
+      ...document.data(),
     };
     result.push(docItem);
   });
   console.log(result);
   return result;
+};
+
+export const setProductsFirebase = async (data : FormInputs) => {
+  const newProductRef = doc(collection(db, 'products'));
+  await setDoc(newProductRef, data);
 };
