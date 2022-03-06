b7f9917892cd2a6981752abbb4dab4b34b1a05be
doremichin <johnkim994@naver.com>
2022-03-06 16:42:16 +0900

    [Chart] donut chart 옵션 적용

--- src/views/chart/components/DonutChart.tsx
+++ src/views/chart/components/DonutChart.tsx
@@ -5,6 +5,25 @@ import Chart from 'react-apexcharts';
 function DonutChart() {
   const options = {
     labels: ['고양이', '강아지', '코뿔소는', '과연', '소인가?'],
+    dataLabels: {
+      enabled: true,
+      formatter(val : any) {
+        return `${Math.round(val)}%`;
+      },
+      dropShadow: {},
+    },
+    plotOptions: {
+      pie: {
+        customScale: 0.9,
+        donut: {
+          labels: {
+            show: true,
+            name: {},
+            value: {},
+          },
+        },
+      },
+    },
   };
   const series = [44, 55, 41, 17, 15];
   const labels = ['A', 'B', 'C', 'D', 'E'];
